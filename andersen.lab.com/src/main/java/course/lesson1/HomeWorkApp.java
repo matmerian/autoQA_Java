93cdb33fc2bcb4cbf8ce3b5d6db353565522de8e
matmerian <matmerian@yandex.ru>
2022-02-05 22:26:24 +0300

    Add Lesson1 HW

---
+++ andersen.lab.com/src/main/java/course/lesson1/HomeWorkApp.java
@@ -0,0 +1,45 @@
+package course.lesson1;
+
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = 0;
+        int b = -34;
+        String answer = a + b >= 0 ? "Сумма положительная" : "Сумма отрицательная";
+        System.out.println(answer);
+    }
+
+    public static void printColor() {
+        int value = -9;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
+
+    public static void compareNumbers() {
+        int a = 15;
+        int b = 5;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+}
